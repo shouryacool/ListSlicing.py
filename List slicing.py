50fe60769c9f467708864e95eba482a087fe0001
shouryacool <84572733+shouryacool@users.noreply.github.com>
2021-10-22 23:15:21 +0530

    Add files via upload

---
+++ List slicing.py
@@ -0,0 +1,20 @@
+#  List Slicing
+friends=["Noob","Harry","Jackie","Jerry","Tom"]
+print(friends[-3:])
+print(friends[-4:])
+
+'''
+ List Methods
+Consider the following list:
+
+  L1 = [1, 8, 7, 2, 21, 15]
+sort() – updates the list to [1,2,7,8,15,21]
+reverse() – updates the list to [15,21,2,7,8,1]
+append(8) – adds 8 at the end of the list
+insert(3,8) – This will add 8 at 3 index
+pop(2) – It will delete the element at index 2 and return its value
+# remove(21) – It will remove 21 from the last
+'''
+
+
+
